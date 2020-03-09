007b9bee77a804ae9fcfe08ad5e5e56f5d7eb740
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2020-03-09 18:01:49 +0200

    Implemented JQuery, other modifications on models.

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Project(models.Model):
     title       = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(max_length=150)
     link        = models.CharField(max_length=300, default="No link provided...")
-    image       = models.ImageField(upload_to='images/', default='No image available...') 
+    image       = models.ImageField(upload_to='images/', default='No image available...')
+    play_link   = models.CharField(max_length=300, default='')
 
     def __str__(self):
         return self.title
